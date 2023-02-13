5a63599e931b326bca11351a3f9b37f51008c96b
Waldo Jaquith <waldo@jaquith.org>
2023-02-13 13:34:54 -0500

    Remove leading tab from first domain

--- bot.sh
+++ bot.sh
@@ -59,7 +59,7 @@ fi
 
 # Turn the list into a post.
 POST_TEXT="The following .gov domains have been registered in the past 24 hours:
-    $DOMAIN_LIST"
+$DOMAIN_LIST"
 
 # Send the message to Mastodon
 curl "$MASTODON_SERVER"/api/v1/statuses -H "Authorization: Bearer ${MASTODON_TOKEN}" -F "status=${POST_TEXT}"
